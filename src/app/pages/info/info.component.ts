aae0f36e1046e1b1d06101af32a7abb0fd24203b
Abdalrahman Abdelaziz <abdalrahmanabdelaziz03@gmail.com>
2024-11-18 18:05:53 +0200

    changes

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrl: './info.component.css'
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit {
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+      
+  }
+
+ 
 
 }
