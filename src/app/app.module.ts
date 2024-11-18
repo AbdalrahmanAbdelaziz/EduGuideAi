f75238c3255cd6cea51fee5fefc82edd35ceb533
Abdalrahman Abdelaziz <abdalrahmanabdelaziz03@gmail.com>
2024-11-18 18:49:08 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
 import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 import { RegisterStudentComponent } from './pages/register-student/register-student.component';
 import { RegisterAdminComponent } from './pages/register-admin/register-admin.component';
 import { StudentPageComponent } from './pages/student-page/student-page.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -41,12 +41,9 @@ import { AdminPageComponent } from './pages/admin-page/admin-page.component';
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
-      timeOut: 5000,
-      positionClass: 'toast-bottom-right', // Position set to bottom-right
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right', 
       newestOnTop: false,
-      preventDuplicates: true,  // Prevents duplicate toastrs from showing
-      closeButton: true,  // Adds a close button
-      progressBar: true,  // Shows a progress bar
     }),
   ],
   providers: [],
