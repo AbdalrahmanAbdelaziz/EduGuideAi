0875cf4057217179133afec6b01f30410ab2a05c
Abdalrahman Abdelaziz <abdalrahmanabdelaziz03@gmail.com>
2024-11-19 00:29:06 +0200

    changes

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../../shared/interfaces/Student';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent  implements OnInit{
+
+
+  student!: Student;
+   capitalizeFirstLetter(name: string): string {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+
+  constructor(private authService: AuthService, private router: Router) {
+
+    this.authService.studentObservable.subscribe((newStudent) => {
+      if (newStudent) {
+        this.student = newStudent; 
+      } 
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+   
+  }
+
+
 
 }
