08563264cf67820767ab3bb3ecec3b52a5e5ed17
Abdalrahman Abdelaziz <abdalrahmanabdelaziz03@gmail.com>
2024-11-18 16:51:35 +0200

    changes

--- src/app/shared/constants/urls.ts
+++ src/app/shared/constants/urls.ts
@@ -5,8 +5,8 @@ export const LOGIN_URL = BASE_URL + '/api/auth/login';
 export const LOGOUT_URL = BASE_URL + '/api/auth/logout';          
 
 
-export const FORGET_PASSWORD_URL = BASE_URL + 'api/auth/forget-password'
-export const RESET_PASSWORD_URL = BASE_URL + 'api/auth/reset-password'
+export const FORGET_PASSWORD_URL = BASE_URL + '/api/auth/forget-password'
+export const RESET_PASSWORD_URL = BASE_URL + '/api/auth/reset-password'
 
 
 export const STUDENT_REGISTER_URL = BASE_URL + '/api/student/register';
