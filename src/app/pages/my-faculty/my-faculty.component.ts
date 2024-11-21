ec4fb4a83a7e79ad8fbcf7e078d6b1070d322aa1
Abdalrahman Abdelaziz <abdalrahmanabdelaziz03@gmail.com>
2024-11-21 22:22:40 +0200

    changes

--- src/app/pages/my-faculty/my-faculty.component.ts
+++ src/app/pages/my-faculty/my-faculty.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Student } from '../../shared/interfaces/Student';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { Course } from '../../shared/interfaces/Course';
+import { UpdateCourse } from '../../shared/interfaces/UpdateCourse';
 
 @Component({
   selector: 'app-my-faculty',
@@ -11,18 +13,50 @@ import { Router } from '@angular/router';
 export class MyFacultyComponent implements OnInit{
 
   student!: Student;
+  allCourses: Course[] = [];
+  coreCourses: Course[] = [];
+  electiveCourses: Course[] = [];
 
-  constructor(private authService: AuthService, private router: Router) {
+  @Output() calculatedHoursEvent = new EventEmitter<number>();
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    
     this.authService.studentObservable.subscribe((newStudent) => {
       if (newStudent) {
         this.student = newStudent;
       }
     });
+
+    this.authService.fetchCourses().subscribe((courses: Course[]) => {
+      this.coreCourses = courses.filter(course => course.type === 'f_core');
+      this.electiveCourses = courses.filter(course => course.type === 'f_elective');
+    });
   }
 
-
-  ngOnInit(): void {
-      
+  canTakeCourse(course: Course): boolean {
+    if (!course.preRequest) return true;
+    const preRequestCourse = this.allCourses.find((c) => c.code === course.preRequest);
+    return preRequestCourse?.grade !== 'none' && preRequestCourse?.grade !== 'F';
   }
 
+  calculateTotalHours(): number {
+    return this.allCourses
+      .filter((course) => course.grade !== 'none')
+      .reduce((total, course) => total + course.hours, 0);
+  }
+
+  submitCourses(): void {
+    const updatedCourses: UpdateCourse[] = this.allCourses.map((course) => ({
+      code: course.code,
+      grade: course.grade || 'none',
+    }));
+
+    this.authService.updateGeneralCourses(updatedCourses).subscribe(() => {
+      const calculatedHours = this.calculateTotalHours();
+      this.calculatedHoursEvent.emit(calculatedHours); 
+    });
+  }
 }
+
--- src/app/shared/interfaces/Course.ts
+++ src/app/shared/interfaces/Course.ts
@@ -5,5 +5,5 @@ export interface Course {
     preRequest?: string; 
     grade?: string; 
     completed?: boolean; 
-    type?: 'g_core' | 'g_elective' | 'f_core' | 'f_elective' | 'd_core' | 'd_elective';
+    type?: 'g_core' | 'g_elective' | 'f_core' | 'f_elective' | 'cs_core' | 'cs_elective' | 'is_core' | 'is_elective' | 'ai_core' | 'ai_elective' | 'it_core' | 'it_elective';
 }
