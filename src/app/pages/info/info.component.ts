7c8c90453c5ebbc2285f246b6050e96216d7a7d3
Abdalrahman Abdelaziz <abdalrahmanabdelaziz03@gmail.com>
2024-11-25 00:46:24 +0200

    changes

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrl: './info.component.css'
+  styleUrls: ['./info.component.css']
 })
 export class InfoComponent implements OnInit {
-  constructor(private router: Router){}
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-      
+    // Scroll event listener
+    this.scrollEvent();
   }
 
- 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.scrollEvent();
+  }
 
+  scrollEvent(): void {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach((section) => {
+      const position = section.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      if (position < windowHeight * 0.8) {
+        section.classList.add('visible');
+      }
+    });
+  }
 }
