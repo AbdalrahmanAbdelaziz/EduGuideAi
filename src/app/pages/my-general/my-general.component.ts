f9ee079fdfe4a554ee6f4cae7ff734ad78a9df24
Abdalrahman Abdelaziz <abdalrahmanabdelaziz03@gmail.com>
2024-11-25 02:25:29 +0200

    changes

--- src/app/pages/my-general/my-general.component.ts
+++ src/app/pages/my-general/my-general.component.ts
@@ -3,20 +3,36 @@ import { AuthService } from '../../services/auth.service';
 import { Course } from '../../shared/interfaces/Course';
 import { Student } from '../../shared/interfaces/Student';
 import { UpdateCourse } from '../../shared/interfaces/UpdateCourse';
-import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 
 @Component({
   selector: 'app-my-general',
   templateUrl: './my-general.component.html',
   styleUrls: ['./my-general.component.css'],
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', [
+        style({ transform: 'translateX(30px)', opacity: 0 }),
+        animate('0.5s ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
+      ])
+    ]),
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1s ease-out', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
+
 export class MyGeneralComponent implements OnInit {
   student!: Student | null;
   allCourses: Course[] = [];
   coreCourses: Course[] = [];
   electiveCourses: Course[] = [];
   selectedCourses: Course[] = [];
+  totalHours: number = 0;
 
   @Output() calculatedHoursEvent = new EventEmitter<number>();
 
